73eb9cab665225a9e1c8ae4d66ffdff27714aac2
Chloe Chen <chloechen080307@gmail.com>
2025-06-06 02:05:50 -0400

    edited

---
+++ src/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '../src/images/Asset 20@4x.png';
+import './Footer.css'
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-component">
+                <div>
+                    <img src={logo} alt="Little Lemon Logo" className="footer-logo" width="75" height="125"/>
+                </div>
+                <div className="contact">
+                    <h4>Contact Us</h4>
+                    <p>Address:123 Chicago Loop, Chicago, IL 60601</p>
+                    <p>Phone: (312) 456-7890</p>
+                    <p>Email: littlelemon123@gmail.com</p>
+                </div>
+                <div className="socials">
+                    <h4>Social Media</h4>
+                    <ul>
+                        <li><a href="https://littlelemon.com">Website</a></li>
+                        <li><a href="https://facebook.com">Facebook</a></li>
+                        <li><a href="https://instagram.com">Instagram</a></li>
+                        <li><a href="https://x.com">X</a></li>
+                        <li><a href="https://linkedin.com">LinkedIn</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../src/images/Logo.svg'
+import './Header.css'
+function Header() {
+    return (
+        <header className="header">
+            <div>
+                <img src={logo} alt="Little Lemon Logo" />
+            </div>
+            <nav className="nav">
+                <ul className="nav-list">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#reservation">Reserve A Table</a></li>
+                    <li><a href="#order">Order Online</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#login">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
