6d81637859b645dfccb7e590e85ead342d04b514
chloec435 <144473427+chloec435@users.noreply.github.com>
2025-06-09 10:20:45 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import BookingPage from './components/BookingPage.js';
 import Home from './components/Home.js';
-import About from './components/About.js';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/booking" element={<BookingPage />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
